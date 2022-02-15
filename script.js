acac533cf5b28c90377580b44d8e16adc27651ed
ForeytKL22 <krforeyt@gmail.com>
2022-02-15 11:57:09 -0700

    got end game and restart functionality working

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-let computerScore = 0;
-let playerScore = 0;
+let computerPoints = 0;
+let playerPoints = 0;
 let roundWinner = '';
 
 let rockBtn = document.getElementById('rock');
 let paperBtn = document.getElementById('paper');
 let scissorsBtn = document.getElementById('scissors');
 let results = document.getElementById('results');
+let userScore = document.querySelector('.user-score');
+let comptuerScore = document.querySelector('.computer-score');
+let endGameDiv = document.querySelector('.end-game');
+let btnDiv = document.createElement('div');
+let restartGame = document.createElement('button');
 
+restartGame.className = "restart-button";
+btnDiv.className = "end-div";
+
+restartGame.textContent = 'Restart';
 
 rockBtn.addEventListener('click', () => playRound('rock', computerPlay));
 paperBtn.addEventListener('click', () => playRound('paper', computerPlay));
 scissorsBtn.addEventListener('click', () => playRound('scissors', computerPlay));
+
+
+restartGame.addEventListener('click', () => clearOut());
+
+
 
 function computerPlay() {
     let computerTurn = Math.floor(Math.random() * 3);
@@ -22,7 +36,6 @@ function computerPlay() {
         return("scissors");
     }
 }
-
 
 function playRound(playerSelection, computerSelection) {
     computerSelection = computerPlay();
@@ -41,34 +54,58 @@ function playRound(playerSelection, computerSelection) {
 } 
 
 function win() {
-    playerScore++;
-    computerScore--;
-    results.textContent = "You win this round! Current score: You: " + `${playerScore}` + "Computer: " + `${computerScore}`
+    playerPoints++;
+    results.textContent = "You win this round!"
+    userScore.textContent = ` ${playerPoints}`
+    comptuerScore.textContent = ` ${computerPoints}`;
+    endGame();
 }
 
 function tie() {
-    results.textContent = "TIE Current score: You: " + `${playerScore}` + "Computer: " + `${computerScore}`
+    results.textContent = "TIE"
+    userScore.textContent = ` ${playerPoints}`
+    comptuerScore.textContent = ` ${computerPoints}`;
+    endGame();
 }
 
 function lose() {
-    playerScore--;
-    computerScore++;
-    results.textContent = "You lose this round! Computer wins! Current score: You: " + `${playerScore}` + "Computer: " + `${computerScore}`
+    computerPoints++;
+    results.textContent = "You lose this round! Computer wins!"
+    userScore.textContent = ` ${playerPoints}`
+    comptuerScore.textContent = ` ${computerPoints}`
+    endGame();
+}
+
+function endGame() {
+    if (playerPoints === 5) {
+        endGameDiv.textContent = "CONGRATS, YOU'VE WON!"
+        restart();
+    } else if (computerPoints === 5) {
+        endGameDiv.textContent = "COMPUTER WINS :("
+        restart();
+    } 
+}
+
+function restart() {
+    endGameDiv.appendChild(btnDiv);
+    btnDiv.appendChild(restartGame);
+}
+
+function clearOut() {
+    computerPoints = 0;
+    playerPoints = 0;
+    userScore.textContent = '';
+    comptuerScore.textContent = '';
+    endGameDiv.textContent = '';
+    results.textContent = '';
 }
 
 
 
-// const buttons = document.querySelectorAll('button');
-
-// buttons.forEach((button) => {
-//     button.addEventListener('click', (e) => {
-        
-//     })
-// })
 
 
-// game();
 
-playRound();
+
+
 
 
