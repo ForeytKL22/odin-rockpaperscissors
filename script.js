69026fda248594f27612dcc9685be14bef19ebac
ForeytKL22 <krforeyt@gmail.com>
2022-02-15 09:28:00 -0700

    got textcontent working and buttons rendering the userSelection

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 let computerScore = 0;
 let playerScore = 0;
+let roundWinner = '';
+
+let rockBtn = document.getElementById('rock');
+let paperBtn = document.getElementById('paper');
+let scissorsBtn = document.getElementById('scissors');
+let results = document.getElementById('results');
+
+
+rockBtn.addEventListener('click', () => playRound('rock', computerPlay));
+paperBtn.addEventListener('click', () => playRound('paper', computerPlay));
+scissorsBtn.addEventListener('click', () => playRound('scissors', computerPlay));
 
 function computerPlay() {
     let computerTurn = Math.floor(Math.random() * 3);
@@ -13,34 +24,51 @@ function computerPlay() {
 }
 
 
-function singleRound(playerSelection, computerSelection) {
-   
-    playerSelection = prompt("Enter rock, paper, or scissors:");
+function playRound(playerSelection, computerSelection) {
     computerSelection = computerPlay();
 
     if (playerSelection === computerSelection) {
-        console.log("TIE!")
+        tie();
     } else if ((playerSelection === "paper" && computerSelection === "rock") ||
     (playerSelection === "rock" && computerSelection === "scissors") ||
     (playerSelection === "scissors" && computerSelection === "paper")) {
-        playerScore++;
-        console.log("You with this round!")
+        win();
     } else if ((playerSelection === "paper" && computerSelection === "scissors") ||
     (playerSelection === "rock" && computerSelection === "paper") ||
     (playerSelection === "scissors" && computerSelection === "rock")) {
-        computerScore++;
-        console.log("You lose this round! Computer wins!")
+        lose();
     } 
 } 
 
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        singleRound();
-        console.log("Computer Score: " + computerScore + " Player Score: " + playerScore);
-    }
+function win() {
+    playerScore++;
+    computerScore--;
+    results.textContent = "You win this round! Current score: You: " + `${playerScore}` + "Computer: " + `${computerScore}`
 }
 
-game();
+function tie() {
+    results.textContent = "TIE Current score: You: " + `${playerScore}` + "Computer: " + `${computerScore}`
+}
+
+function lose() {
+    playerScore--;
+    computerScore++;
+    results.textContent = "You lose this round! Computer wins! Current score: You: " + `${playerScore}` + "Computer: " + `${computerScore}`
+}
+
+
+
+// const buttons = document.querySelectorAll('button');
+
+// buttons.forEach((button) => {
+//     button.addEventListener('click', (e) => {
+        
+//     })
+// })
+
+
+// game();
+
+playRound();
 
 
